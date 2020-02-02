d33a48328358ecb353ed63e4ac984bb71d8a9487
golden1345 <37267119+golden1345@users.noreply.github.com>
2020-02-02 21:30:15 +0300

    React-Router

--- react-site/src/components/Header.js
+++ react-site/src/components/Header.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "./logo192.png";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "../pages/Home"
+import News from "../pages/News"
+import Contacts from "../pages/Contacts"
+import Sale from "../pages/Sale"
+
 
 export default class Header extends Component {
   render() {
@@ -21,13 +27,23 @@ export default class Header extends Component {
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="mr-auto">
                 <Nav.Link href="/"> Главная </Nav.Link>
-                <Nav.Link href="/about"> Новости </Nav.Link>
+                <Nav.Link href="/news"> Новости </Nav.Link>
                 <Nav.Link href="/contacts"> Контакты </Nav.Link>
                 <Nav.Link href="/sale"> Каталог </Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
+
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/news" component={News} />
+            <Route exact path="/contacts" component={Contacts} />
+            <Route exact path="/sale" component={Sale} />
+          </Switch>
+        </Router>
+
       </>
     );
   }
---
+++ react-site/src/pages/Contacts.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Contacts extends Component {
+    render() {
+        return (
+            <div>
+                +7 960
+            </div>
+        )
+    }
+}
