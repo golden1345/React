55b6ab4f8ddf691881403d8ccb205782726e2004
golden1345 <37267119+golden1345@users.noreply.github.com>
2020-02-04 17:16:19 +0300

    React and Okta

---
+++ auth/client/src/app.config.js
@@ -0,0 +1,6 @@
+export default {
+    url: 'https://{dev-158147.okta.com}',
+    issuer: 'https://{dev-158147.okta.com}/oauth2/default',
+    redirect_uri: window.location.origin + '/implicit/callback',
+    client_id: '{0oa1a75dom3ytD9Hp4x6}'
+  }
